7b7709d780ccf4f4ef42bfec0c4b90f8476125ac
Ammy <54329554+Ammy@users.noreply.github.com>
2020-10-07 18:46:59 -0500

    Add files via upload

---
+++ src/Movie.js
@@ -0,0 +1,9 @@
+var MovieName = "Fate/Stay Night: Heaven's Feel - I. Presage Flower/ II. Lost Butterfly/ III. spring song";
+var MoviePRating ="Hurts but I love it.";
+
+    function display(name,PRating)
+    {
+        alert(name+"\n"+PRating);
+    }
+
+    display(MovieName,MoviePRating);
